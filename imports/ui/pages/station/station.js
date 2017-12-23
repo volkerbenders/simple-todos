479349d6570fee3af2dd5c90a0bd09f399a46900
Volker <vbenders@gmail.com>
2017-12-23 16:07:15 +0100

    render adress string for

--- imports/ui/pages/station/station.js
+++ imports/ui/pages/station/station.js
@@ -10,7 +10,8 @@ Template.Station_data.onCreated(function () {
 })
 
 Template.Station_data.helpers({
-  stations () {
+  /*
+  station () {
     console.log('get id from flowrouter')
     const id = FlowRouter.getParam('_id')
     console.log('got id <' + id + '>')
@@ -19,5 +20,28 @@ Template.Station_data.helpers({
     console.log('got station <' + station + '>')
     console.log('got station <' + JSON.stringify(station) + '>')
     return station
+  },
+  */
+  stationName () {
+    console.log('get id from flowrouter')
+    const id = FlowRouter.getParam('_id')
+    console.log('got id <' + id + '>')
+
+    const station = Stations.findOne(id)
+    console.log('got station <' + station + '>')
+    console.log('got station <' + JSON.stringify(station) + '>')
+    return station.result[0].name
+  },
+  address () {
+    const id = FlowRouter.getParam('_id')
+    const station = Stations.findOne(id)
+    const mailingAddress = station.result[0].mailingAddress
+
+    console.log('Mailng Adress: ' + mailingAddress)
+    console.log('Mailng Adress: ' + JSON.stringify(mailingAddress))
+    var address = mailingAddress.street + ', ' + mailingAddress.zipcode + ' ' + mailingAddress.city
+    console.log('Mailng Adress: ' + address)
+    //return 'volker war hioer' //JSON.stringify(address)
+    return address
   }
 })
